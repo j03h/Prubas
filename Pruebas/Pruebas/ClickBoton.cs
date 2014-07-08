6414a0233c5ca6e3070fe2e02086859c5fd410a1
unknown <jescobar@JESCOBAR2.vtholding.cl>
2014-07-08 11:20:36 -0400

    Inicio

---
+++ Pruebas/Pruebas/ClickBoton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pruebas
+{
+    class ClickBoton
+    {
+        public static void Click()
+        {
+            Form1 Form = new Form1();
+
+            Form.textBox1.Text = "Pruebas";
+            Form.Refresh();
+        }
+    }
+}
